dd630a2d08b187f79fe5571b86d2da148fff9220
衞藤卓也 <otheranother490@gmail.com>
2019-07-15 17:32:00 +0900

    設定変更

--- ticket_notify/settings.py
+++ ticket_notify/settings.py
@@ -14,9 +14,21 @@ import os
 import logging
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/' # localでcssを読み込むためのパス(たぶん)
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'), # サーバでcssを読み込むためのパス
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # 管理サイトのログイン機能を通常のログイン機能として使う
 LOGIN_URL='admin:login'
